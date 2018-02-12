881a01e9ad0e674f352aa886817b39e29c36625f
FlashMath1 <35616682+FlashMath1@users.noreply.github.com>
2018-02-12 17:27:25 -0600

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("You have left "+attempt+" attempts;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
