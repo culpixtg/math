f6e5c1301a7c0beed3ae5e82dde5d51f5e79d6ea
FlashMath1 <35616682+FlashMath1@users.noreply.github.com>
2018-02-12 17:09:42 -0600

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "Formget" && password == "formget#123"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "FlashMath1.github.io/Menu.html"; // Redirecting to other page.
 return false;
 }
 else{
