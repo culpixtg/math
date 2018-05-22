0e660cf2c36bb6d7818dc186e4f2655ca61d8546
FlashMath1 <35616682+FlashMath1@users.noreply.github.com>
2018-05-22 02:25:15 +0000

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -1,40 +1,12 @@
 <html>  
 <script src="//inc.freefind.com/inc/ffse-overlay.min.js" async></script>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {margin:0;}
-.navbar {
-  overflow: hidden;
-  background-color: #333;
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-.navbar a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-.navbar a:hover {
-  background: #ddd;
-  color: black;
-}
-.main {
-  padding: 16px;
-  margin-top: 30px;
-  height: 1500px; /* Used in this example to enable scrolling */
-}
-</style>
+
 </head>
 <body>
 
-<div class="navbar">
-  <?php include('nav_bar.html'); ?>
-</div>
+<div id="navbar"></div>
 
 <div class="main">
 
@@ -45,6 +17,8 @@ body {margin:0;}
   
 
 </div>
-
+<script>
+  $("#navbar").load("nav_bar.html"); 
+  </script>
 </body>
 </html>
